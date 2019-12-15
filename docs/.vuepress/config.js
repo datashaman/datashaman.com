e61d49d991bf480af7faa5c5ecba570253194312
datashaman <marlinf@datashaman.com>
2019-12-15 10:57:15 +0200

    wip

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,74 @@
+module.exports = {
+    head: [
+        ['link', {
+            rel: 'me',
+            href: 'https://twitter.com/data_shaman',
+        }],
+        ['link', {
+            rel: 'me',
+            href: 'https://github.com/datashaman',
+        }],
+        ['link', {
+            rel: 'me',
+            href: 'https://www.linkedin.com/in/datashaman',
+        }],
+    ],
+    theme: '@vuepress/blog',
+    themeConfig: {
+        footer: {
+            copyright: [
+                {
+                    text: '© 2019 datashaman',
+                    link: 'https://github.com/datashaman',
+                }, {
+                    text: 'Powered by VuePress',
+                    link: 'https://vuepress.vuejs.org/',
+                }
+            ],
+            contact: [
+                {
+                    type: 'twitter',
+                    link: 'https://twitter.com/data_shaman',
+                }, {
+                    type: 'github',
+                    link: 'https://github.com/datashaman',
+                }, {
+                    type: 'linkedin',
+                    link: 'https://www.linkedin.com/in/datashaman',
+                },
+            ],
+        },
+        nav: [
+            {
+                text: 'Home',
+                link: '/',
+            }, {
+                text: 'Tags',
+                link: '/tags/',
+            }
+        ],
+    },
+    plugins: [
+        ['@vuepress/blog', {
+            directories: [
+                {
+                    id: 'post',
+                    dirname: '_posts',
+                    path: '/',
+                    itemLayout: 'Post',
+                    itemPermalink: '/:year/:month/:day/:slug',
+                },
+            ],
+            frontmatters: [
+                {
+                    id: "tag",
+                    keys: ['tag', 'tags'],
+                    path: '/tags/',
+                    layout: 'Tag',
+                    frontmatter: { title: 'Tag' },
+                    itemlayout: 'Tag',
+                },
+            ],
+        }],
+    ],
+}
---
+++ docs/about.md
@@ -0,0 +1,13 @@
+---
+title: about
+description: about me
+meta:
+    - name: description
+      content: about me
+    - name: keywords
+      content: me
+---
+
+# about
+
+[home](/)
