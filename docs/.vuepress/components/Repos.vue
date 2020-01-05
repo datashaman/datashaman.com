6abc293f8c22316402aa1e86c922ec88eab816eb
datashaman <marlinf@datashaman.com>
2020-01-05 10:02:00 +0200

    Add repos

---
+++ docs/.vuepress/components/Repos.vue
@@ -0,0 +1,71 @@
+<template>
+  <ul class="repos">
+    <li v-for="repo in repos" :key="repo.id" class="repo">
+      <h2>
+        <a :href="repo.html_url">{{ repo.name }}</a>
+        <Icon v-if="repo.fork" name="code-branch"/>
+      </h2>
+
+      {{ repo.parent }}
+      {{ repo.source }}
+
+      <ul class="meta">
+        <li><Icon name="clock"/> Updated {{ vagueTime(new Date(repo.updated_at)) }}</li>
+        <li v-if="repo.fork && repo.parent"><Icon name="code-branch"/> Forked from <a :href="repo.parent.url">{{ repo.parent.name }}</a></li>
+      </ul>
+
+      <p v-if="repo.description">{{ repo.description }}</p>
+    </li>
+  </ul>
+</template>
+
+<script>
+import 'vue-awesome/icons/clock'
+import 'vue-awesome/icons/code-branch'
+import Icon from 'vue-awesome/components/Icon'
+
+import Octokit from '@octokit/rest'
+import vagueTime from 'vague-time'
+
+export default {
+  components: {
+    Icon,
+  },
+  created () {
+    const octokit = new Octokit()
+    octokit.repos
+      .listForUser({
+        sort: 'updated',
+        type: 'owner',
+        username: 'datashaman',
+      })
+      .then(({ data }) => {
+        this.repos = data.filter((repo) => {
+          return !repo.archived
+        })
+      })
+  },
+  data () {
+    return {
+      repos: [],
+    }
+  },
+  methods: {
+    vagueTime (dt) {
+      return vagueTime.get({
+        to: dt,
+      })
+    },
+  },
+}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+}
+.meta {
+  font-size: smaller;
+  font-weight: 200;
+}
+</style>
